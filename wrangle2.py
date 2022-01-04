e14c5af6985ad33c4bb923f49a6edfe115e1eca4
Jared Godar <jared.godar@gmail.com>
2022-01-04 16:39:10 -0600

    add wrangle2

--- wrangle2.py
+++ wrangle2.py
@@ -1,18 +1,33 @@
-####### IMPORT LIBRARIES ############
 
-
-import numpy as np
+# Basic libraries
+from env import host, user, password  # Database credentials
+import wrangle
 import pandas as pd
+import numpy as np
+import numpy as np
+
+# Vizualization Tools
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import env
+# Modeling Tools
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import statsmodels.api as sm
 
+from datetime import date
 
+import sklearn.preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-################ PULL DATA FROM DB ##############
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Custim functions
+
+# URL from ENV
 
 
 def get_db_url(database):
@@ -23,8 +38,9 @@ def get_db_url(database):
     url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
     return url
 
+# Query
 
-# acquire zillow data using the query
+
 def get_zillow():
     query = '''
     SELECT prop.*, 
@@ -59,32 +75,7 @@ def get_zillow():
     df = pd.read_sql(query, get_db_url('zillow'), index_col='id')
     return df
 
-
-#### INITIAL MVP DROP THEM ALL MISSING VALUES FUNCTION #########
-
-def handle_missing_values(df, prop_required_column=.5, prop_required_row=.70):
-    threshold = int(round(prop_required_column*len(df.index), 0))
-    df.dropna(axis=1, thresh=threshold, inplace=True)
-    threshold = int(round(prop_required_row*len(df.columns), 0))
-    df.dropna(axis=0, thresh=threshold, inplace=True)
-    return df
-
-
-# DROP COLUMNS
-
-def remove_columns(df, cols_to_remove):
-    '''
-    Pass a list od columns to remove
-    '''
-    df = df.drop(columns=cols_to_remove)
-    return df
-
-
-def import_csv():
-    '''
-    Brings in previous stored spreadsheet
-    '''
-    df = pd.read_csv('zillow.csv')
+    # Pull values from SQL
 
 
 def single_use(df):
@@ -102,13 +93,27 @@ def add_county(df):
     '''
     Add column for counties
     '''
+    import numpy as np
     df['county'] = np.where(df.fips == 6037, 'Los_Angeles',
                             np.where(df.fips == 6059, 'Orange',
                                      'Ventura'))
-    # drop columns not needed
-    df = remove_columns(df, ['id',
-                             'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid', 'propertycountylandusecode', 'propertylandusetypeid', 'propertyzoningdesc',
-                             'censustractandblock', 'propertylandusedesc', 'heatingorsystemdesc', 'unitcnt', 'buildingqualitytypeid'])
+    return df
+
+
+def handle_missing_values(df, prop_required_column=.5, prop_required_row=.70):
+    threshold = int(round(prop_required_column*len(df.index), 0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns), 0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+
+
+def remove_columns(df, cols_to_remove):
+    '''
+    Pass a list od columns to remove
+    '''
+    df = df.drop(columns=cols_to_remove)
+    return df
 
 
 def clean(df):
@@ -122,29 +127,17 @@ def clean(df):
     return df
 
 
-def min_max_scaler(train, valid, test):
+def remove_outliers(df, col_list, k=1.5):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
     '''
-    Uses the train & test datasets created by the split_my_data function
-    Returns 3 items: mm_scaler, train_scaled_mm, test_scaled_mm
-    This is a linear transformation. Values will lie between 0 and 1
-    '''
-    num_vars = list(train.select_dtypes('number').columns)
-    scaler = MinMaxScaler(copy=True, feature_range=(0, 1))
-    train[num_vars] = scaler.fit_transform(train[num_vars])
-    valid[num_vars] = scaler.transform(valid[num_vars])
-    test[num_vars] = scaler.transform(test[num_vars])
-    return scaler, train, valid, test
-
-
-def outlier_function(df, cols, k):
-    # function to detect and handle oulier using IQR rule
-    for col in df[cols]:
-        q1 = df.annual_income.quantile(0.25)
-        q3 = df.annual_income.quantile(0.75)
-        iqr = q3 - q1
-        upper_bound = q3 + k * iqr
-        lower_bound = q1 - k * iqr
-        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
+    for col in col_list:
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        iqr = q3 - q1   # calculate interquartile range
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+        # return dataframe without outliers
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
     return df
 
 
@@ -162,20 +155,70 @@ def split_my_data(df, pct=0.10):
     return train, validate, test
 
 
+def min_max_scaler(train, validate, test):
+    '''
+    Uses the train & test datasets created by the split_my_data function
+    Returns 3 items: mm_scaler, train_scaled_mm, test_scaled_mm
+    This is a linear transformation. Values will lie between 0 and 1
+    '''
+    num_vars = list(train.select_dtypes('number').columns)
+    scaler = MinMaxScaler(copy=True, feature_range=(0, 1))
+    train[num_vars] = scaler.fit_transform(train[num_vars])
+    validate[num_vars] = scaler.transform(validate[num_vars])
+    test[num_vars] = scaler.transform(test[num_vars])
+    return scaler, train, validate, test
+
+
+def add_baseline(train, validate, test):
+    '''
+    Assigns mean error as baseline prediction
+    '''
+    baseline = train.logerror.mean()
+    train['baseline'] = baseline
+    validate['baseline'] = baseline
+    test['baseline'] = baseline
+    return train, validate, test
+
+
+def split_xy(train, validate, test):
+    '''
+    Splits dataframe into train, validate, and test data frames
+    '''
+    X_train = train.drop(columns='logerror')
+    y_train = train.logerror
+
+    X_validate = validate.drop(columns='logerror')
+    y_validate = validate.logerror
+
+    X_test = test.drop(columns='logerror')
+    y_test = test.logerror
+
+    return train, X_train, y_train, X_validate, y_validate, X_test, y_test
+
+
+def scale(X_train, X_validate, X_test, train, validate, test):
+    '''
+    Uses the train & test datasets created by the split_my_data function
+    Returns 3 items: mm_scaler, train_scaled_mm, test_scaled_mm
+    This is a linear transformation. Values will lie between 0 and 1
+    '''
+    num_vars = list(X_train.select_dtypes('number').columns)
+    scaler = MinMaxScaler(copy=True, feature_range=(0, 1))
+    train[num_vars] = scaler.fit_transform(X_train[num_vars])
+    validate[num_vars] = scaler.transform(X_validate[num_vars])
+    test[num_vars] = scaler.transform(X_test[num_vars])
+    return X_train, X_validate, X_test
+
+
 def wrangle():
-    #df = get_zillow()
-    # df.to_csv('unedited_zillow.csv')
     df = pd.read_csv('unedited_zillow.csv')
     df = single_use(df)
     df = add_county(df)
     df = handle_missing_values(df)
     df = clean(df)
-    #columns=['bathroomcnt', 'bedroomcnt', 'calculatedbathnbr', 'calculatedfinishedsquarefeet', 'finishedsquarefeet12', 'fullbathcnt', 'lotsizesquarefeet', 'roomcnt', 'unitcnt','structuretaxvaluedollarcnt', 'taxvaluedollarcnt','taxamount']
-    #df = remove_outliers(df, columns)
     train, validate, test = split_my_data(df)
     train, validate, test = add_baseline(train, validate, test)
     train, X_train, y_train, X_validate, y_validate, X_test, y_test = split_xy(
         train, validate, test)
     X_train, X_validate, X_test = scale(
         X_train, X_validate, X_test, train, validate, test)
-    return train, X_train, y_train, X_validate, y_validate, X_test, y_test
