d5a7ad98f3be7602ac801454cbcf057932388b5f
Jared Godar <jared.godar@gmail.com>
2022-01-05 10:14:43 -0600

    Update feature engineering and binning in wrangle

--- wrangle3.py
+++ wrangle3.py
@@ -124,7 +124,7 @@ def clean(df):
     return df
 
 
-def remove_outliers(df, col_list, k=1.5):
+def remove_outliers1(df, col_list, k=1.5):
     ''' remove outliers from a list of columns in a dataframe 
         and return that dataframe
     '''
@@ -207,13 +207,87 @@ def scale(X_train, X_validate, X_test, train, validate, test):
     return X_train, X_validate, X_test
 
 
+def create_features(df):
+    df['age'] = 2017 - df.yearbuilt
+    df['age_bin'] = pd.cut(df.age,
+                           bins=[0, 5, 10, 20, 30, 40, 50, 60, 70,
+                                 80, 90, 100, 110, 120, 130, 140],
+                           labels=[0, .066, .133, .20, .266, .333, .40, .466, .533,
+                                   .60, .666, .733, .8, .866, .933])
+
+    # create taxrate variable
+    df['taxrate'] = df.taxamount/df.taxvaluedollarcnt*100
+
+    # create acres variable
+    df['acres'] = df.lotsizesquarefeet/43560
+
+    # bin acres
+    df['acres_bin'] = pd.cut(df.acres, bins=[0, .10, .15, .25, .5, 1, 5, 10, 20, 50, 200],
+                             labels=[0, .1, .2, .3, .4, .5, .6, .7, .8, .9])
+
+    # square feet bin
+    df['sqft_bin'] = pd.cut(df.calculatedfinishedsquarefeet,
+                            bins=[0, 800, 1000, 1250, 1500, 2000,
+                                  2500, 3000, 4000, 7000, 12000],
+                            labels=[0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                            )
+
+    # dollar per square foot-structure
+    df['structure_dollar_per_sqft'] = df.structuretaxvaluedollarcnt / \
+        df.calculatedfinishedsquarefeet
+
+    df['structure_dollar_sqft_bin'] = pd.cut(df.structure_dollar_per_sqft,
+                                             bins=[0, 25, 50, 75, 100, 150,
+                                                   200, 300, 500, 1000, 1500],
+                                             labels=[
+                                                 0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                             )
+
+    # dollar per square foot-land
+    df['land_dollar_per_sqft'] = df.landtaxvaluedollarcnt/df.lotsizesquarefeet
+
+    df['lot_dollar_sqft_bin'] = pd.cut(df.land_dollar_per_sqft, bins=[0, 1, 5, 20, 50, 100, 250, 500, 1000, 1500, 2000],
+                                       labels=[
+                                           0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                       )
+
+    # update datatypes of binned values to be float
+    df = df.astype({'sqft_bin': 'float64', 'acres_bin': 'float64', 'age_bin': 'float64',
+                    'structure_dollar_sqft_bin': 'float64', 'lot_dollar_sqft_bin': 'float64'})
+
+    # ratio of bathrooms to bedrooms
+    df['bath_bed_ratio'] = df.bathroomcnt/df.bedroomcnt
+
+    # 12447 is the ID for city of LA.
+    # I confirmed through sampling and plotting, as well as looking up a few addresses.
+    df['cola'] = df['regionidcity'].apply(lambda x: 1 if x == 12447.0 else 0)
+
+    return df
+
+
+def remove_outliers(df):
+    '''
+    remove outliers in bed, bath, zip, square feet, acres & tax rate
+    '''
+
+    return df[((df.bathroomcnt <= 7) & (df.bedroomcnt <= 7) &
+               (df.regionidzip < 100000) &
+               (df.bathroomcnt > 0) &
+               (df.bedroomcnt > 0) &
+               (df.acres < 20) &
+               (df.calculatedfinishedsquarefeet < 10000) &
+               (df.taxrate < 10)
+               )]
+
+
 def wrangle():
     df = pd.read_csv('zillow_wrangle3.csv')
     df = single_use(df)
     df = clean(df)
     df = add_county(df)
     df = handle_missing_values(df)
-    #df = df.dropna()
+    df = create_features(df)
+    df = remove_outliers(df)
     train, validate, test = split_my_data(df)
     train, validate, test = add_baseline(train, validate, test)
     train, X_train, y_train, X_validate, y_validate, X_test, y_test = split_xy(
